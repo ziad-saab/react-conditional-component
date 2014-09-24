3bc126b170387d82c3671a68ba7513fae35227b5
Ziad Saab <ziad.saab@gmail.com>
2014-09-24 13:15:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# [React](http://facebook.github.io/react/)-conditional-component
+
+This component displays its children only if they satisfy their predicate:
+
+```html
+<Conditional value={someValue}>
+  <span showIfEquals={10}>The value is 10 or "10"</span>
+  <span showIfStrictlyEquals={10}>The value is 10</span>
+  <span showIfLte={10}>The value is lower than or equal to 10</span>
+  ...
+</Conditional>
+```
+
+## Install
+
+```sh
+bower install react-conditional-component
+or
+npm install react-conditional-component
+```
+
+Simply require it to use it:
+
+```javascript
+var Conditional = require('react-conditional-component');
+```
+
+## Example
+
+```html
+/**
+* @jsx React.DOM
+*/
+var Demo = React.createClass({
+  getInitialState: function() {
+    return {someValue: 10};
+  },
+
+  componentDidMount: function() {
+    // Change the value after 1 second
+    setTimeout(function() {
+      this.setState({value: 0});
+    }.bind(this), 1000);
+  },
+
+  render: function() {
+    return (
+      <Conditional value={this.state.someValue}>
+        <span showIfEquals={10}>The value is 10 or "10"</span>
+        <span showIfStrictlyEquals={10}>The value is 10</span>
+        <span showIfLte={10}>The value is lower than or equal to 10</span>
+        <span showIfTrue>The value is true</span>
+        <span showIfTruthy>The value is truthy</span>
+      </Conditional>
+    );
+  }
+});
+
+React.renderComponent(<Demo/>, document.body);
+```
+
+## License
+
+MIT.
---
+++ react-conditional-component.jsx
@@ -0,0 +1,53 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    var value = this.props.value;
+    var shown = [];
+    this.props.children.forEach(function(child) {
+      var show = true;
+      if (show && child.props.showIfDefined) {
+        show = typeof value !== 'undefined';
+      }
+      if (show && child.props.showIfUndefined) {
+        show = typeof value === 'undefined';
+      }
+      if (show && child.props.showIfTrue) {
+        show = value === true;
+      }
+      if (show && child.props.showIfTruthy) {
+        show = !!value;
+      }
+      if (show && child.props.showIfFalse) {
+        show = value === false;
+      }
+      if (show && child.props.showIfFalsy) {
+        show = !value;
+      }
+      if (show && typeof child.props.showIfEquals !== 'undefined') {
+        show = value == child.props.showIfEquals;
+      }
+      if (show && typeof child.props.showIfStrictlyEquals !== 'undefined') {
+        show = value === child.props.showIfStrictlyEquals;
+      }
+      if (show && typeof child.props.showIfLte !== 'undefined') {
+        show = value <= child.props.showIfLte;
+      }
+      if (show && typeof child.props.showIfLt !== 'undefined') {
+        show = value < child.props.showIfLt;
+      }
+      if (show && typeof child.props.showIfGte !== 'undefined') {
+        show = value >= child.props.showIfGte;
+      }
+      if (show && typeof child.props.showIfGt !== 'undefined') {
+        show = value > child.props.showIfGt;
+      }
+
+      if (show) {
+        shown.push(child);
+      }
+    });
+
+    return <div>{shown}</div>;
+  }
+});
